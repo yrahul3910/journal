4e80c907f470a62579392bb3d5258729232102ad
Rahul Yedida <yrahul3910@gmail.com>
2018-01-24 21:07:05 +0530

    Fixed #22

--- app/injectEmoji.js
+++ app/injectEmoji.js
@@ -1,15 +1,24 @@
 const fs = require("fs");
 const path = require("path");
 
+// Disabled no-unused-vars since ESLint won't pick up that
+// we used it inside a template literal.
+const addEmoji = (name) => { // eslint-disable-line no-unused-vars
+    $("textarea").val((i, text) => { // eslint-disable-line no-undef
+        return text + `:${name}:`;
+    });
+};
+
 exports.injectEmojis = (selector) => {
     fs.readdir(path.join(__dirname, "../emoji"), (err, files) => {
         if (err) throw err;
         let html = "<div class='row'>";
         files.forEach((file) => {
+            let emoji_name = file.split(".")[0];
             html += `
                 <div class="col-md-3">
                     <img class="emoji injected-emoji" src="../emoji/${file}" \
-                        onclick="$('textarea').append(':${file.split(".")[0]}:')" />
+                        onclick="addEmoji('${emoji_name}')" />
                 </div>
             `;
         });
